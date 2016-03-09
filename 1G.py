1c15863ab4047034cb4f844c3e73d4b96ee0321b
EVE YUAN <eve.yi.yuan@gmail.com>
2016-03-09 12:01:04 -0800

    there are still bugs

--- 1G.py
+++ 1G.py
@@ -1,15 +1,18 @@
 import numpy as np
 import random as rand
+from shakespeare import parse, parseTok
 
 
 def main():
     eps = 0.01
-
+    token_vals, obs_seq = parseTok('shakespeare.txt', 'spenser.txt')
+    num_obs = len(obs_seq)
+    num_states = 7
     A = np.zeros((num_states, num_states))
 
     # randomly initialize A matrix
-    for i in range(num_states)
-        for j in range(num_states)
+    for i in range(num_states):
+        for j in range(num_states):
             A[i][j] = rand.random()
         # make each row sum to 1
         A[i][:] = A[i][:] / np.sum(A[i][:])
@@ -17,8 +20,8 @@ def main():
     O = np.zeros((num_states, num_obs))
 
     # randomly initialize O matrix
-    for i in range(num_states)
-        for j in range(num_obs)
+    for i in range(num_states):
+        for j in range(num_obs):
             O[i][j] = rand.random()
         # make each row sum to 1
         O[i][:] = O[i][:] / np.sum(O[i][:])
@@ -30,7 +33,7 @@ def main():
     prev_diff = diff
     prev_A = A_
 
-    while diff/prev_diff > eps
+    while diff/prev_diff > eps:
         prev_diff = diff
         gamma, xi = eStep(num_states, obs_seq, A, O)
         A, O = mStep(gamma, xi, obs_seq, num_obs)
@@ -57,59 +60,58 @@ def latex_matrix(matrix):
 # Uses a single Maximization step to compute A (state-transition) and
 # O (observation) matrices. See Lecture 6 Slide 65.
 def mStep(gamma, xi, obs, num_obs):
-
-	A = [[0. for i in num_states] for j in num_states]
+    A = [[0. for i in num_states] for j in num_states]
     O = [[0. for i in num_states] for j in num_obs]
 
-    for i in range(num_states)
-        for j in range(num_states)
+    for i in range(num_states):
+        for j in range(num_states):
             A[i][j] = np.sum(xi[:][:end-1][i][j]) / np.sum(gamma[:][:end-1][i])
 
-        for j in range(num_obs)
-            for o in range(len(obs))
-                for t in range(obs_len)
-                    if obs[o][t] == j
+        for j in range(num_obs):
+            for o in range(len(obs)):
+                for t in range(obs_len):
+                    if obs[o][t] == j:
                         O[i][j] += gamma[o][t][i]
             O[i][j] /= np.sum(gamma[:][:][i]) 
 
     return A, O
 
 def eStep(num_states, obs_seq, A, O):
-
-	# probability of being in state i at time t given the observed sequence
+    # probability of being in state i at time t given the observed sequence
     # and parameters
+    obs_len = len(obs_seq)
     gamma = [[[0. for i in range(num_states)] for j in range(obs_len)] for k in range(len(obs_seq))]
 
     # probability of being in state i and j at time t
     xi = [[[[0. for i in range(num_states)] for j in range(num_states)] for k in range(obs_len)] for l in range(len(obs_seq))]
 
-	for obs_num in len(obs_seq)
-	    obs = obs_seq[obs_num]
+    for obs_num in range(len(obs_seq)):
+        obs = obs_seq[obs_num]
 
-		alpha = forward(num_states, obs, A, O)
-		beta = backward(num_states, obs, A, O)
-		
+        alpha = forward(num_states, obs, A, O)
+        beta = backward(num_states, obs, A, O)
+        
 
-		# now we compute the marginals
-		obs_len = len(obs)
-	    num_states = len(alpha[0])
-
-
-	    for length in range(obs_len)
-
-	        den = np.sum(alpha[length] * beta[length])
-	        for state in range(num_states)
-	            gamma[obs_num][length][state] = alpha[length][state] * beta[length][state] / den
+        # now we compute the marginals
+        obs_len = len(obs)
+        num_states = len(alpha[0])
 
 
+        for length in range(obs_len):
 
-	    den = np.sum(alpha[end][:])
-	    for t in range(obs_len)
-		    for i in range(num_states)
-		        for j in range(num_states)
-		            xi[obs_num][t][i][j] = alpha[t][i] * A[i][j] * beta[t+1][j] * O[j][obs[t+1]] / den
+            den = np.sum(alpha[length] * beta[length])
+            for state in range(num_states):
+                gamma[obs_num][length][state] = alpha[length][state] * beta[length][state] / den
 
-	return gamma, xi
+
+
+        den = np.sum(alpha[end][:])
+        for t in range(obs_len):
+            for i in range(num_states):
+                for j in range(num_states):
+                    xi[obs_num][t][i][j] = alpha[t][i] * A[i][j] * beta[t+1][j] * O[j][obs[t+1]] / den
+
+    return gamma, xi
 
 
 
@@ -156,7 +158,7 @@ def forward(num_states, obs, A, O):
 def backward(num_states, obs, A, O):
     len_ = len(obs)                   # number of observations
     # stores p(seqence)
-    prob = [[[1] for i in range(num_states)] for i in range(len_)]
+    prob = [[1 for i in range(num_states)] for i in range(len_)]
 
     for length in range(len_-2, -1, -1):   # length + 1 to avoid initial condition
         for state in range(num_states):
--- shakespeare.py
+++ shakespeare.py
@@ -1,8 +1,8 @@
 # Parses the text files for Shakespeare and Spenser's sonnets and runs the
 # program.
-
 shakespeare = 'shakespeare.txt'
 spenser = 'spenser.txt'
+
 
 # Note that the files are parsed into the following fowm: it is a nested
 # list of lists of lines: the outmost list is a list of poems, which
@@ -28,21 +28,78 @@ def parse(filename):
 			poem.append(line)
 	return corpus
 
+def parseTok(f1, f2):
+	token_vals = []
+	observations = []
+	shak = parse(f1)
+	spen = parse(f2)
+	for poem in shak:
+		for line in poem:
+			for word in line.split():
+	 			if word not in token_vals:
+	 				token_vals.append(word.lower())
+	for poem in spen:
+		for line in poem:
+	 		for word in line.split():
+	 			if word not in token_vals:
+	 				token_vals.append(word.lower())
+
+	for poem in shak:
+		temp = []
+		for line in poem:
+			for word in line.split():
+				temp.append(token_vals.index(word.lower()))
+		
+		observations.append(temp)
+
+	for poem in spen:
+		temp = []
+		for line in poem:
+			for word in line.split():
+				temp.append(token_vals.index(word.lower()))
+		observations.append(temp)
+	return token_vals, observations
+
+
 if __name__ == '__main__':
 	# Test to see if parsing works.
+	token_vals = []
+	observations = []
 	shak = parse(shakespeare)
 	print "Shakespeare"
 	print len(shak)
 	print '\n'
 	for poem in shak:
 		for line in poem:
-	 		print line
+			for word in line.split():
+	 			if word not in token_vals:
+	 				token_vals.append(word.lower())
+	 				
 	spen = parse(spenser)
 	print "Spenser"
 	print len(spen)
 	print '\n'
-	for poem in shak:
+	for poem in spen:
 		for line in poem:
-	 		print line
+	 		for word in line.split():
+	 			if word not in token_vals:
+	 				token_vals.append(word.lower())
+	
+	for poem in shak:
+		temp = []
+		for line in poem:
+			for word in line.split():
+				temp.append(token_vals.index(word.lower()))
+		
+		observations.append(temp)
+
+	for poem in spen:
+		temp = []
+		for line in poem:
+			for word in line.split():
+				temp.append(token_vals.index(word.lower()))
+		observations.append(temp)
+
+	print observations
 	#print len(shak)
 	#print len(spen)
