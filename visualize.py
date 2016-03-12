1cdee0e312d52eb2fb6043f865d0c65e1e758fcc
EVE YUAN <eve.yi.yuan@gmail.com>
2016-03-12 10:51:45 -0800

    filter out punctuation in visualization

--- visualize.py
+++ visualize.py
@@ -1,11 +1,13 @@
 from nltk.corpus import wordnet as wn
+import string
 def visualize(type, O_Mat, tokens):
 	if type == "noun":
 		nouns = {x.name().split('.', 1)[0] for x in wn.all_synsets('n')}
 		for state in range(0, len(O_Mat)):
 			prob = 0
 			for obs in range(0, len(O_Mat[0])):
-				if tokens[obs] in nouns:
+				word = tokens[obs].translate(None, ".()'?,:!")
+				if word in nouns:
 					prob += O_Mat[state][obs]
 			print "State " + str(state) + " Noun Prob: " + str(prob)
 	if type == "verb":
@@ -13,6 +15,7 @@ def visualize(type, O_Mat, tokens):
 		for state in range(0, len(O_Mat)):
 			prob = 0
 			for obs in range(0, len(O_Mat[0])):
-				if tokens[obs] in verbs:
+				word = tokens[obs].translate(None, ".()'?,:!")
+				if word in verbs:
 					prob += O_Mat[state][obs]
 			print "State " + str(state) + " Verb Prob: " + str(prob)
